bd20521b0250dd6c71c2eee080535ef4ae332390
Florentino Ruiz Aquino <fruizaquino@gmail.com>
2026-01-20 13:47:36 -0600

    commit con error
    esto es un commit con error

--- SitioWebCarrera/src/app/app.ts
+++ SitioWebCarrera/src/app/app.ts
@@ -1,15 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {NavbarSharedComponent} from './shared/components/navbar/navbar-shared.component'
-import {FooterSharedComponent} from './shared/components/footer/footer-shared.component'
-
-
+import {NavbarSharedComponent} from './shared/components/navbar/navbar-shared.component';
+import {FooterSharedComponets } from "./shared/components/footer/footer-shared.component";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarSharedComponent, FooterSharedComponent],
+  imports: [RouterOutlet, NavbarSharedComponent, FooterSharedComponets, HeaderSharedComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('SitioWebCarrera');
+  protected readonly title = signal('In.En Desarrollo de Software y Sistemas Inteligentes-Informática');
 }
