d96289adab26e098ff8d9f0b8158a512b3b9080c
nashe0209 <113204052+nashe0209@users.noreply.github.com>
2026-01-26 10:58:07 -0600

    Brenda - Proyectos
    Funciones de cards

--- SitioWebCarrera/src/app/pages/proyectos/proyectos-page.component.ts
+++ SitioWebCarrera/src/app/pages/proyectos/proyectos-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'proyecto',
@@ -6,4 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./proyectos-page.component.css']
 })
 
-export class ProyectoPageComponent {}
+export class ProyectoPageComponent {
+
+}
+
