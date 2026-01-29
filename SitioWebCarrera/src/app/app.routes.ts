2031792696935ee8a02c8823ed591a97ac0cf8df
Gawalek <113203529+Gawalek@users.noreply.github.com>
2026-01-29 08:21:16 -0600

    Fernando-Inicio
    Se crearon las paginas de los 3 recuadros (excelencia,tecnologia y vinculacion ) se agrego el paartado de noticas y el calendario

--- SitioWebCarrera/src/app/app.routes.ts
+++ SitioWebCarrera/src/app/app.routes.ts
@@ -1,8 +1,43 @@
 import { Routes } from '@angular/router';
+<<<<<<< Updated upstream
 // Importamos SOLO el inicio para que cargue rápido al entrar
 import { InicioPageComponent } from './pages/inicio/inicio-pages.component';
 
 export const routes: Routes = [
+=======
+<<<<<<< HEAD
+import { InicioPageComponent } from './pages/inicio/inicio-pages.component';
+import { QuienesSomosPageComponent } from './pages/quienesSomos/quienesSomos-page.component';
+import { PlanEstudiosPageComponent } from './pages/planEstudio/PlanEstudio-page.component';
+import { ProyectoPageComponent } from './pages/proyectos/proyectos-page.component';
+import { DocentesPageComponent } from './pages/docentes/docentes-pages.component';
+import { AspirantesPageComponent } from './pages/aspirantes/aspirantes-pages.component';
+
+// Importamos los componentes nuevos
+import { ExcelenciaComponent } from './pages/inicio/excelencia/excelencia.component';
+import { TecnologiaComponent } from './pages/inicio/tecnologia/tecnologia.component';
+import { VinculacionComponent } from './pages/inicio/vinculacion/vinculacion.component'; // 👈 NUEVO
+
+export const routes: Routes = [
+  { path: '', component: InicioPageComponent },
+  { path: 'quienesSomos', component: QuienesSomosPageComponent },
+  { path: 'planEstudios', component: PlanEstudiosPageComponent },
+  { path: 'proyectos', component: ProyectoPageComponent },
+  { path: 'docentes', component: DocentesPageComponent },
+  { path: 'aspirantes', component: AspirantesPageComponent },
+
+  // Rutas de las tarjetas del inicio
+  { path: 'excelencia-academica', component: ExcelenciaComponent },
+  { path: 'tecnologia-avanzada', component: TecnologiaComponent },
+  { path: 'vinculacion-regional', component: VinculacionComponent }, // 👈 NUEVA RUTA
+
+  { path: '**', redirectTo: '' }
+=======
+// Importamos SOLO el inicio para que cargue rápido al entrar
+import { InicioPageComponent } from './pages/inicio/inicio-pages.component';
+
+export const routes: Routes = [
+>>>>>>> Stashed changes
   {
     path: '',
     component: InicioPageComponent
@@ -37,4 +72,8 @@ export const routes: Routes = [
     path: '**',
     redirectTo: ''
   }
+<<<<<<< Updated upstream
+=======
+>>>>>>> aa27c8fbcd9c8e07324b2f1a819034c201f31d01
+>>>>>>> Stashed changes
 ];
